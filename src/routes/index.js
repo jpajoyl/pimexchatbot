9f8df08b110834ec6dfe0daff695403a3f6fdc10
Juan Manuel Pajoy <jpajoy@pimex.co>
2021-03-30 09:17:18 -0500

    add lead to pimex

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-const { Router } = require('express');
+const {Router} = require('express');
+const axios = require('axios');
 const router = Router();
-
 
 
 router.get('/', ((req, res) => {
@@ -10,6 +10,21 @@ router.get('/', ((req, res) => {
 }))
 router.post('/chatterbot', ((req, res) => {
     console.log(req.body)
+    const api = {
+        "url": "https://api.pimex.io/v2/",
+        "url1": "https://api.pimex.io/",
+        "key": "!eqBwMfoWmEOWRoz^R^60$p2K"
+    }
+    const data = {
+        _state: "lead",
+        email: "juan@gmail.com",
+        name: "Juan",
+        origin: "https://app.pimex.co",
+        phone: "12344",
+        project: "14557",
+        referrer: "test"
+    }
+    axios.post('https://api.pimex.io/v2/conversions/', data).then(r => console.log(r))
     res.json({
         "fulfillmentMessages": [
             {
